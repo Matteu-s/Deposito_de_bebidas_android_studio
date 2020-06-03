5879b1cf2beb5d9268fdf5cd3b5d369bafe604bd
Mateus <mateeussantoos@hotmail.com>
2020-06-03 02:28:53 -0300

    tentando fazer os botoes dos grupos de produto funcionar, sem sucesso

--- app/src/main/java/com/mateussantosti/protocolodearromba/ui/produtos/ProdutosFragment.java
+++ app/src/main/java/com/mateussantosti/protocolodearromba/ui/produtos/ProdutosFragment.java
@@ -1,35 +1,36 @@
-package com.mateussantosti.protocolodearromba.ui.Produtos;
+package com.mateussantosti.protocolodearromba.ui.produtos;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.mateussantosti.protocolodearromba.R;
 
 public class ProdutosFragment extends Fragment {
 
-    private ProdutosViewModel dashboardViewModel;
-
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        dashboardViewModel =
                 ViewModelProviders.of(this).get(ProdutosViewModel.class);
         View root = inflater.inflate(R.layout.fragment_produtos, container, false);
-        final TextView textView = root.findViewById(R.id.text_dashboard);
-        dashboardViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
         return root;
+    }
+}
+
+class produtoRefrigerante extends AppCompatActivity{
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_produtos);
+    }
+    public void proximaTela(View view){
+        Intent intent = new Intent(this, produtoRefrigerante.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/mateussantosti/protocolodearromba/ui/produtos/ProdutosViewModel.java
@@ -0,0 +1,7 @@
+package com.mateussantosti.protocolodearromba.ui.produtos;
+
+import androidx.lifecycle.ViewModel;
+
+public class ProdutosViewModel extends ViewModel {
+
+}
